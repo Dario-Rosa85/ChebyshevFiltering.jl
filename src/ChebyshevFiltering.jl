efa4e5df7a258762e1f222efc501703a3fb5f590
Dario-Rosa85 <dario.rosa85@gmail.com>
2023-07-05 17:46:29 +0900

    ready for the KPM estimate

--- src/ChebyshevFiltering.jl
+++ src/ChebyshevFiltering.jl
@@ -6,5 +6,6 @@ using SparseArrays
 export renormalization_hamiltonian!
 
 include(joinpath(@__DIR__, "hamiltonian_rescaling/renormalization_hamiltonian.jl"))
+include(joinpath(@__DIR__, "search_vectors_degree_polynomials/optimal_search_degree.jl"))
 
 end # module ChebyshevFiltering
