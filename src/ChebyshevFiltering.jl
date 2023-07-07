f99196fa3fa072724e627798f06eb027e0f15766
Dario-Rosa85 <dario.rosa85@gmail.com>
2023-07-07 13:40:38 +0900

    file structure reorganized

--- src/ChebyshevFiltering.jl
+++ src/ChebyshevFiltering.jl
@@ -2,10 +2,11 @@ module ChebyshevFiltering
 
 using KrylovKit
 using SparseArrays
+using Polynomials
 
 export renormalization_hamiltonian!
 
 include(joinpath(@__DIR__, "hamiltonian_rescaling/renormalization_hamiltonian.jl"))
-include(joinpath(@__DIR__, "search_vectors_degree_polynomials/optimal_search_degree.jl"))
+include(joinpath(@__DIR__, "KPM_density/KPM_density.jl"))
 
 end # module ChebyshevFiltering
